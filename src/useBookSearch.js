353f7f2a01361dececcd1111a28223d0a7698833
Mark Packham <markpackham1@gmail.com>
2019-10-02 14:35:31 +0100

    setting conditions for searches https://youtu.be/NZKUirTtxcg?t=821

--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -2,7 +2,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function useBookSearch(query, pageNumber) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [books, setBooks] = useState([]);
+  /* prevent from making unwanted requests, especially if what we want can't be found */
+  const [hasMore, setHasMore] = useState(false);
+
   useEffect(() => {
+    setLoading(true), setError(false);
     let cancel;
     axios({
       method: "GET",
@@ -10,6 +17,9 @@ export default function useBookSearch(query, pageNumber) {
       params: { q: query, page: pageNumber },
       cancelToken: new axios.CancelToken(c => (cancel = c))
     }).then(res => {
+      setBooks(prevBooks => {
+        return [...prevBooks, res.data.docs.map(b => b.title)];
+      });
       console.log(res.data);
     });
     return () =>
